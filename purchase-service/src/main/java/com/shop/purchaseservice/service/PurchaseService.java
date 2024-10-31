3eade1429801c0038006436a2d8de78981ef9c23
ya <nikita6969228@gmail.com>
2024-10-31 21:30:43 +0200

    31.10 tests fixing purchase DIVIDE METHOD

--- purchase-service/src/main/java/com/shop/purchaseservice/service/PurchaseService.java
+++ purchase-service/src/main/java/com/shop/purchaseservice/service/PurchaseService.java
@@ -3,9 +3,7 @@ package com.shop.purchaseservice.service;
 import com.shop.purchaseservice.client.CustomerClient;
 import com.shop.purchaseservice.client.StorageClient;
 import com.shop.purchaseservice.dto.*;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -33,31 +31,42 @@ public class PurchaseService {
         InventoryStatusDTO inventoryStatusDTO = new InventoryStatusDTO();
 
         if (storageClient.isOrderInStorage(orderWithProductCartDTO.getCart())) {
-            log.info("Order is in storage, sending to Kafka topic.");
-            kafkaAddOrder.send("order-topic", orderWithProductCartDTO);
-            purchaseMailSend(orderWithProductCartDTO);
-            customerClient.cleanCart(orderWithProductCartDTO.getCustomerId());
 
-            if (orderWithProductCartDTO.getCost().compareTo(new BigDecimal("500.0")) > 0) {
-                SaleDuplicateDTO saleDuplicateDTO = SaleDuplicateDTO.builder()
-                        .sale(new BigDecimal("0.05"))
-                        .customerId(orderWithProductCartDTO.getCustomerId())
-                        .build();
-                kafkaSale.send("sale-topic", saleDuplicateDTO);
-                log.info("Sale sent to Kafka for customerId {}: {}", orderWithProductCartDTO.getCustomerId(), saleDuplicateDTO);
-            }
+            purchaseLogicIfOrderInStorage(orderWithProductCartDTO);
+
             inventoryStatusDTO.setIsOrderInStorage(true);
             log.info("Inventory status updated: {}", inventoryStatusDTO);
 
         } else {
-            log.warn("Order is not in storage, finding out of stock products.");
-            Map<ProductDuplicateDTO, Integer> outOfStorage = storageClient.findOutOfStorageProduct(
-                    orderWithProductCartDTO.getCart(), orderWithProductCartDTO.getCustomerId());
+
+            inventoryStatusDTO.setOutOfStorageProducts(purchaseLogicIfOrderNotInStorage(orderWithProductCartDTO));
             inventoryStatusDTO.setIsOrderInStorage(false);
-            inventoryStatusDTO.setOutOfStorageProducts(outOfStorage);
-            log.info("Out of storage products found: {}", outOfStorage);
         }
         return inventoryStatusDTO;
+    }
+
+    public void purchaseLogicIfOrderInStorage(OrderWithProductCartDTO orderWithProductCartDTO) {
+        log.info("Order is in storage, sending to Kafka topic.");
+        kafkaAddOrder.send("order-topic", orderWithProductCartDTO);
+        purchaseMailSend(orderWithProductCartDTO);
+        customerClient.cleanCart(orderWithProductCartDTO.getCustomerId());
+
+        if (orderWithProductCartDTO.getCost().compareTo(new BigDecimal("500.0")) > 0) {
+            SaleDuplicateDTO saleDuplicateDTO = SaleDuplicateDTO.builder()
+                    .sale(new BigDecimal("0.05"))
+                    .customerId(orderWithProductCartDTO.getCustomerId())
+                    .build();
+            kafkaSale.send("sale-topic", saleDuplicateDTO);
+            log.info("Sale sent to Kafka for customerId {}: {}", orderWithProductCartDTO.getCustomerId(), saleDuplicateDTO);
+        }
+    }
+
+    public Map<ProductDuplicateDTO, Integer> purchaseLogicIfOrderNotInStorage(OrderWithProductCartDTO orderWithProductCartDTO) {
+        log.warn("Order is not in storage, finding out of stock products.");
+        Map<ProductDuplicateDTO, Integer> outOfStorage = storageClient.findOutOfStorageProduct(
+                orderWithProductCartDTO.getCart(), orderWithProductCartDTO.getCustomerId());
+        log.info("Out of storage products found: {}", outOfStorage);
+        return outOfStorage;
     }
 
     public void purchaseMailSend(OrderWithProductCartDTO orderWithProductCartDTO) {
@@ -66,6 +75,12 @@ public class PurchaseService {
         CustomerDTO customerDTO = customerClient.findCustomerEmailAndNameById(customerId);
         List<String> listOfProducts = new ArrayList<>();
 
+        Map<ProductDuplicateDTO, Integer> cart = orderWithProductCartDTO.getCart();
+
+        for (Map.Entry<ProductDuplicateDTO, Integer> entry : cart.entrySet()) {
+            listOfProducts.add(entry.getKey().getName());
+        }
+
         Map<String, Object> data = Map.of(
                 "Cost", orderWithProductCartDTO.getCost(),
                 "ID", orderWithProductCartDTO.getId(),
@@ -73,11 +88,6 @@ public class PurchaseService {
                 "Name", customerDTO.getName()
         );
 
-        Map<ProductDuplicateDTO, Integer> cart = orderWithProductCartDTO.getCart();
-
-        for (Map.Entry<ProductDuplicateDTO, Integer> entry : cart.entrySet()) {
-            listOfProducts.add(entry.getKey().getName());
-        }
 
         MailDTO mailDTO = MailDTO.builder()
                 .to(customerDTO.getEmail())
